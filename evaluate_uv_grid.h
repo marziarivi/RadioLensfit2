ca870f3a5108ab7815b6818e52fdeed00f5ad35d
Marzia Rivi <marzia.rivi@gmail.com>
2024-07-14 11:42:23 +0200

    Update evaluate_uv_grid.h
    manage PSF weighting

--- evaluate_uv_grid.h
+++ evaluate_uv_grid.h
@@ -28,10 +28,10 @@ extern "C" {
     
 //double weight_func(double u, double v);
 unsigned int facet_size(double theta_med, double len);
-unsigned int evaluate_uv_grid_size(int rank, int nprocs, double len, double *wavenumbers, unsigned int num_channels, unsigned  int ncoords, double* u, double* v, unsigned int sizeg, bool* flag, unsigned long int* count);
-unsigned int evaluate_facet_coords(double* grid_u, double* grid_v, double len, unsigned int sizeg, unsigned long int *count);
-void gridding_visibilities(double *wavenumbers, unsigned int num_channels, unsigned int ncoords, double *u, double *v, complexd *vis, double *sigma2, double len, unsigned int sizeg, complexd *new_vis, double *new_sigma2, bool* flag, unsigned long int* count);
-void average_facets(unsigned long int size, complexd* grid_vis, double* grid_sigma2 ,unsigned long int *count);
+unsigned int evaluate_uv_grid_size(int rank, int nprocs, double len, double *wavenumbers, unsigned int num_channels, unsigned  int ncoords, double* u, double* v, unsigned int sizeg, bool* flag);
+unsigned int evaluate_facet_coords(double* grid_u, double* grid_v, double len, unsigned int sizeg, double *count_w);
+void gridding_visibilities(double *wavenumbers, unsigned int num_channels, unsigned int ncoords, double *u, double *v, complexd *vis, double *sigma2, double len, unsigned int sizeg, complexd *new_vis, double *new_sigma2, bool* flag, double* sum_w);
+void average_facets(unsigned long int size, complexd* grid_vis, double* grid_sigma2 , double *sum_w);
     
 //void gridding_visibilities_sinc(unsigned long int ncoords, double *u, double *v, complexd *vis, double len, int sizeg, complexd *new_vis, double *count);
 //void convolve_with_sinc(unsigned long int ncoords, double *u, double *v, complexd *vis, double fov, double wavelength, complexd *new_vis);
